706b46f72c4444898277a91d0d5c0ea8e6638060
Tomoyuki Sakurai <y@trombik.org>
2023-07-28 13:58:43 +0700

    bugfix: warn depecated header file

---
+++ components/eil_helpers/esp_idf_lib_helpers.h
@@ -0,0 +1,5 @@
+#if !defined(__ESP_IDF_LIB_HELPERS__H__)
+#define __ESP_IDF_LIB_HELPERS__H__
+#warn "esp_idf_lib_helpers.h is deprecated, use eil_helpers.h instead"
+#include "eil_helpers.h"
+#endif
